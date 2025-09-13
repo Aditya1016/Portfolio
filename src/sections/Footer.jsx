2086adadea2bd20de44be15e52bc81abcbf1d6eb
Aditya Sinha <iamaditya.sinha16@gmail.com>
2025-09-13 21:34:01 +0530

    Update Footer.jsx
    minor tweaks

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = () => {
         </div>
         <div className="flex gap-3">
             <div className="social-icon">
-                <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2"/>
+                <a><img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2"/></a>
             </div>
-            <div className="social-icon">
+{/*             <div className="social-icon">
                 <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2"/>
             </div>
             <div className="social-icon">
                 <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2"/>
-            </div>
+            </div> */}
         </div>
 
-        <p className="text-white-500">&copy; 2024 Adrian All Rights Reserved</p>
+        <p className="text-white-500">&copy; 2024 All Rights Reserved</p>
     </section>
   )
 }
